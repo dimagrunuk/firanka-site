c4c00106399fd3342797e463b972740f3e67724e
dimagrunuk <grinikdmitrij@gmail.com>
2026-03-23 15:48:56 +0200

    beforecontact

--- js/script.js
+++ js/script.js
@@ -10,11 +10,11 @@ loadSection("footer", "sections/footer/footer.html");
 loadSection("advantages", "sections/advantages/advantages.html");
 loadSection("catalog", "sections/catalog/catalog.html");
 loadSection("banner", "sections/banner/banner.html");
-loadSection("history", "sections/history/history.html");
+loadSection("reviews", "sections/reviews/reviews.html");
+loadSection("instagram", "sections/instagram/instagram.html");
 loadSection("products", "sections/products/products.html");
 loadSection("portfolio", "sections/portfolio/portfolio.html");
-loadSection("contact", "sections/contact/contact.html");
-let slides = document.querySelectorAll(".slide");
+
 let index = 0;
 
 function startSlider() {
@@ -33,3 +33,16 @@ function startSlider() {
 
 /* ЧЕКАЄМО ПОКИ ПІДВАНТАЖИТЬСЯ HERO */
 setTimeout(startSlider, 500);
+const track = document.querySelector('.reviews-track');
+const dots = document.querySelectorAll('.dot');
+
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        const slide = dot.dataset.slide;
+
+        track.style.transform = `translateX(-${slide * 100}%)`;
+
+        dots.forEach(d => d.classList.remove('active'));
+        dot.classList.add('active');
+    });
+});
