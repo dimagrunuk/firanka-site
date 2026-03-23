1735cae33d68d36a7a45941d55cc78d40dd58866
dimagrunuk <grinikdmitrij@gmail.com>
2026-03-23 13:00:29 +0200

    1hero

--- js/script.js
+++ js/script.js
@@ -7,3 +7,29 @@ async function loadSection(id, file) {
 loadSection("header", "sections/header/header.html");
 loadSection("hero", "sections/hero/hero.html");
 loadSection("footer", "sections/footer/footer.html");
+loadSection("advantages", "sections/advantages/advantages.html");
+loadSection("catalog", "sections/catalog/catalog.html");
+loadSection("banner", "sections/banner/banner.html");
+loadSection("history", "sections/history/history.html");
+loadSection("products", "sections/products/products.html");
+loadSection("portfolio", "sections/portfolio/portfolio.html");
+loadSection("contact", "sections/contact/contact.html");
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function startSlider() {
+    let slides = document.querySelectorAll(".slide");
+    let index = 0;
+
+    setInterval(() => {
+        slides[index].classList.remove("active");
+
+        index++;
+        if (index >= slides.length) index = 0;
+
+        slides[index].classList.add("active");
+    }, 3000);
+}
+
+/* ЧЕКАЄМО ПОКИ ПІДВАНТАЖИТЬСЯ HERO */
+setTimeout(startSlider, 500);
